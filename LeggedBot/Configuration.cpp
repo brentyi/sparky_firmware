e1beb8a4e99b2dff66963dc900b04685c01525d0
Brent Yi <yibrenth@gmail.com>
2016-11-29 01:22:17 -0800

    implemented push/pull commands for robot config

--- LeggedBot/Configuration.cpp
+++ LeggedBot/Configuration.cpp
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "Configuration.h"
+#include <EEPROM.h>
 
 Configuration::Configuration() {
   return;
@@ -9,12 +10,12 @@ Configuration::Configuration() {
  * @brief Pull in saved configuration.
 */
 void Configuration::pull() {
-
+  EEPROM.get(0, data);
 }
 
 /**
  * @brief Push current configuration.
 */
 void Configuration::push() {
-
+  EEPROM.put(0, data);
 }
